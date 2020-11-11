e7d87b2b27242ef68f577586d3e3382426b33045
Emily Mulhall <emilymulhall@Emilys-MacBook-Air.local>
2020-11-11 15:52:02 -0600

    Dataset fully created for first time

--- create_dataset.py
+++ create_dataset.py
@@ -1,36 +1,27 @@
 import pandas as pd
 import numpy as np
-import copy
+import datetime
 
 
 def create_dataset():
 	#Load the csv data from our two different sources
-	mobility_dataframe = pd.read_csv('2020_US_Region_Mobility_Report.csv', infer_datetime_format=True, parse_dates=True)
-	cdc_dataframe = pd.read_csv('United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv', infer_datetime_format=True, parse_dates=True)
+	mobility_df = pd.read_csv('2020_US_Region_Mobility_Report.csv')
+	cdc_df = pd.read_csv('United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
 
 	#clean mobility data
 	#get rid of the country_region_code, country_region, metro_area, iso_3166_2_code,census_fips_code
-	mobility_dataframe=mobility_dataframe.drop(columns=['country_region_code', 'country_region', 'metro_area',  'iso_3166_2_code', 'census_fips_code'])
+	mobility_df=mobility_df.drop(columns=['country_region_code', 'country_region', 'metro_area',  'iso_3166_2_code', 'census_fips_code'])
 	#get rid of first few hundred rows, which are average across the entire US, where sub_region_1='nan'
-	mobility_dataframe=mobility_dataframe.dropna(subset=['sub_region_1'])
+	mobility_df=mobility_df.dropna(subset=['sub_region_1'])
 	#because we want the average by state we need to get rid of rows which are split out by sub_region_2
-	mobility_dataframe=mobility_dataframe[mobility_dataframe['sub_region_2'].isnull()]
+	mobility_df=mobility_df[mobility_df['sub_region_2'].isnull()]
 	#now sub_region_2 is of no use, so we can remove it
-	mobility_dataframe=mobility_dataframe.drop(columns=['sub_region_2'])
+	mobility_df=mobility_df.drop(columns=['sub_region_2'])
+	#let's change our dates to a datetime for future comparisons
+	mobility_df['date']=pd.to_datetime(mobility_df['date'], format='%Y-%m-%d')
 
-	#clean cdc data for sanity purposes - we are going to be interested in
-	#total cases
-	#new_cases
-	cdc_dataframe=cdc_dataframe.drop(columns=['created_at', 'consent_cases', 'consent_deaths', 'prob_cases', 'prob_death', 'tot_death', 
-		'conf_death','new_death','pnew_death', 'conf_cases'])
-	#remove dates before the start of our mobility data
-	cdc_dataframe=cdc_dataframe[cdc_dataframe['submission_date']>='02/15/2020']
-	#remove dates after the end of our mobility data
-	cdc_dataframe=cdc_dataframe[cdc_dataframe['submission_date']<='11/06/2020']
-	#print(cdc_dataframe.max())
-	#print(mobility_dataframe.max())
-	#print(cdc_dataframe.shape)
-	#print(mobility_dataframe.shape)
+	#let's also change our dates in cdc data to a datetime for future comparisons
+	cdc_df['submission_date']=pd.to_datetime(cdc_df['submission_date'], format='%m/%d/%Y')
 
 	#In mobility data states are listed by full list name, but in CDC data states are listed by abbreviation
 	#Use the below dictionary to match
@@ -92,6 +83,27 @@ def create_dataset():
     	'Wisconsin': 'WI',
     	'Wyoming': 'WY'
 	}
+
+	#Build y_value column
+	new_cases_per_state_per_day=[]
+	for index, row in mobility_df.iterrows():
+		date=row['date']
+		state=state_dict[row['sub_region_1']]
+
+		match=cdc_df[(cdc_df['submission_date']==date)&(cdc_df['state']==state)]
+		#sanity check
+		if(len(match==1)):
+			#We will treat nan as a 0 when adding up these results
+			new_cases=match['new_case'].fillna(0)
+			pnew_cases=match['pnew_case'].fillna(0)
+			new_cases_per_state_per_day.append(float(new_cases)+float(pnew_cases))
+		else:
+			new_cases_per_state_per_day.append(0)
+
+	mobility_df['num_cases']=new_cases_per_state_per_day
+
+	mobility_df.to_csv('COVID-19_Combined_Mobility_And_Infection_Data.csv', index=False)
+
 
 if __name__ == '__main__':
 	#Create dataset
