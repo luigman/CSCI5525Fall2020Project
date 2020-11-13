ed2d34945019a44a2fc1d5c205fff017360b860a
Luis Guzman <luigman@gmail.com>
2020-11-13 17:59:38 -0600

    Reorganized, added some comments

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -9,7 +9,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, Ridge, RidgeCV
 
 def baseline():
     showPlot = True
@@ -18,10 +18,7 @@ def baseline():
     mobility_dataframe = pd.read_csv('google_baseline_test.csv', infer_datetime_format=True, parse_dates=True)
     cdc_dataframe = pd.read_csv('cdc_baseline_test_movingAvg.csv', infer_datetime_format=True, parse_dates=True)
 
-    full_dataframe = pd.concat([mobility_dataframe, cdc_dataframe], axis=1)
-
-    #sns.pairplot(full_dataframe[['newAndPnew', 'retail_and_recreation_percent_change_from_baseline', 'workplaces_percent_change_from_baseline', 'residential_percent_change_from_baseline']], diag_kind='kde')
-    #plt.show()
+    #=========================FIND BEST OFFSET========================================
 
     bestLinearCorr = 0
     bestLogCorr = 0
@@ -81,6 +78,7 @@ def baseline():
         plt.title("Logarithmic correlation vs. data offset")
         plt.show()
 
+        #Plot data correlations
         #sns.pairplot(bestLinearData[['newAndPnew','retail_and_recreation_percent_change_from_baseline', 'grocery_and_pharmacy_percent_change_from_baseline', 'parks_percent_change_from_baseline', 'workplaces_percent_change_from_baseline', 'residential_percent_change_from_baseline','originalCases']], diag_kind='kde')
         #plt.show()
 
@@ -94,6 +92,8 @@ def baseline():
     print("Best Log Correlation:", bestLogCorr)
     print("Best Log Correlation Norm:", np.linalg.norm(bestLogCorr))
     print("Best Log Offset:", bestLogOffset)
+
+    #=========================BEGIN MODEL FITTING========================================
 
     linearMSE = []
     logMSEAdj = []
@@ -111,18 +111,22 @@ def baseline():
 
     for t in range((min(bestLinearData.shape[0], bestLogData.shape[0])-60)//stride):
         print("Training model:",t)
+
+        #Linear Mobility Data
         linearTrainX = bestLinearData[t*stride:t*stride+30,1:]
         linearTrainy = bestLinearData[t*stride:t*stride+30,:1]
-        logTrainy2 = np.log(linearTrainy+1)
-        logTrainX = bestLogData[t*stride:t*stride+30,1:]
-        logTrainy = bestLogData[t*stride:t*stride+30,:1]
-        linearCasesTrainX = linearCasesOnly[t*stride:t*stride+30]
-        logCasesTrainX = logCasesOnly[t*stride:t*stride+30]
-
         linearTestX = bestLinearData[t*stride+30:t*stride+60,1:]
         linearTesty = bestLinearData[t*stride+30:t*stride+60,:1]
+
+        #Logarithmic Mobility Data
+        logTrainX = bestLogData[t*stride:t*stride+30,1:]
+        logTrainy = bestLogData[t*stride:t*stride+30,:1]
         logTestX = bestLogData[t*stride+30:t*stride+60,1:]
         logTesty = bestLogData[t*stride+30:t*stride+60,:1]
+
+        #Cases-only data
+        linearCasesTrainX = linearCasesOnly[t*stride:t*stride+30]
+        logCasesTrainX = logCasesOnly[t*stride:t*stride+30]
         linearCasesTestX = linearCasesOnly[t*stride+30:t*stride+60]
         logCasesTestX = logCasesOnly[t*stride+30:t*stride+60]
 
@@ -135,22 +139,21 @@ def baseline():
         #linearTestX = np.hstack((linearTestX, timeTest))
         #logTestX = np.hstack((logTestX, timeTest))
 
-
         #fit linear model
-        linear_model = Ridge(alpha=1.0).fit(linearTrainX, linearTrainy)
+        linear_model = RidgeCV(cv=3).fit(linearTrainX, linearTrainy)
 
         predict = linear_model.predict(linearTestX)
         linearMSE.append(np.abs(predict-linearTesty)/linearTesty)
 
         #fit log model
-        linear_model = Ridge(alpha=1.0).fit(logTrainX, logTrainy)
+        linear_model = RidgeCV(cv=3).fit(logTrainX, logTrainy)
 
         predict = linear_model.predict(logTestX)
         predictAdj = np.exp(predict)-1+np.min(full_dataframe_noDate.to_numpy()) #convert from log back to raw case number
         logMSEAdj.append(np.abs(predictAdj-linearTesty)/linearTesty)
 
         #fit linear cases only model
-        cases_model = LinearRegression().fit(timeTrain, linearCasesTrainX)
+        cases_model = RidgeCV(cv=3).fit(timeTrain, linearCasesTrainX)
         if showPlot:
           visualize_cases(cases_model, timeTrain, linearCasesTrainX, timeTest, linearCasesTestX)
 
@@ -158,15 +161,13 @@ def baseline():
         linearCasesMSE.append(np.abs(predict-linearCasesTestX)/linearCasesTestX)
 
         #fit log cases only model
-        cases_model = LinearRegression().fit(np.log(timeTrain), logCasesTrainX)
+        cases_model = RidgeCV(cv=3).fit(np.log(timeTrain), logCasesTrainX)
         if showPlot:
           visualize_cases(cases_model, np.log(timeTrain), logCasesTrainX, np.log(timeTest), logCasesTestX)
 
         predict = cases_model.predict(np.log(timeTest))
         predictAdj = np.exp(predict)-1 #convert from log back to raw case number
         logCasesMSE.append(np.abs(predictAdj-linearCasesTestX)/linearCasesTestX)
-
-
 
     plt.plot(np.array(linearMSE).mean(axis=0), label='Mobility (linear, non-temporal)')
     plt.plot(np.array(logMSEAdj).mean(axis=0), label='Mobility (logarithmic, non-temporal)')
